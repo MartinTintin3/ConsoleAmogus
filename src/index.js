b6d5edf794b72d5699b330f5b178418d7cbace73
Martin M <maroymart@gmail.com>
2021-07-10 00:40:38 -0400

    Why error

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 const skeldjs = require('@skeldjs/client');
 
+(async () => {
+	const client = new skeldjs.SkeldjsClient('‎2021.6.15');
+
+	await client.connect('NA', 'meeee');
+	console.info('Connected to server');
+})();
